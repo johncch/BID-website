fe17eb8c2b9db65bfcebec673502f341fdbb8baa
Chong Han <johncch@gmail.com>
2011-02-16 18:47:25 -0500

    Midway

--- js/main.js
+++ js/main.js
@@ -1,26 +1,205 @@
+debug = false;
 $(function() {
 		paging.init();
 		var self = this;
 		var fsm = new FSM({
-							firstState: 'login',
 							display: $("div#iPhone"),
 							states: [
 										new State({
-												img_src: 'images/a_login.png',
-												id: 'login',
+												img_src: 'images/assigner/00_appstart.png',
+												id: 'app_start',
 												clickableDivs: [
-												   createClickableDiv(195, 558, 84, 46, 'taskmenu'),
+													createClickableDiv(201, 262, 66, 63, 'login'),
 												]
 											}),
 										new State({
-												img_src: 'images/b_taskmenu.png',
-												id: 'taskmenu',
+												img_src: 'images/assigner/a_login.png',
+												id: 'login',
 												clickableDivs: [
+													createClickableDiv(48, 465, 296, 28, 'login_type'),
 												]
 											}),
+										new State({
+												img_src: 'images/assigner/a_login  (TYPE).png',
+												id: 'login_type',
+												clickableDivs: [
+													createClickableDiv(279, 324, 66, 24, 'taskmenu'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/b_taskmenu.png',
+												id: 'taskmenu',
+												clickableDivs: [
+													createClickableDiv(44, 557, 307, 44, 'newtask'),
+													createClickableDiv(47, 158, 51, 29, 'edit_taskmenu'),
+													createClickableDiv(295, 159, 54, 30, 'settings'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/b_taskmenu EDIT.png',
+												id: 'edit_taskmenu',
+												clickableDivs: [
+													createClickableDiv(44, 557, 307, 44, 'newtask'),
+													createClickableDiv(47, 158, 51, 29, 'taskmenu'),
+													createClickableDiv(295, 159, 54, 30, 'settings'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/c_newtask.png',
+												id: 'newtask',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'taskmenu'),
+													createClickableDiv(61,212,274,44, 'mover'),
+													createClickableDiv(58,272,279,45, 'driver'),
+													createClickableDiv(62,331,273,48, 'custodian'),
+													createClickableDiv(62,391,275,43, 'cashier'),
+													createClickableDiv(62,453,273,40, 'handyman'),
+													createClickableDiv(62,512,273,41, 'typist'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_mover.png',
+												id: 'mover',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_driver.png',
+												id: 'driver',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_custodian.png',
+												id: 'custodian',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_cashier.png',
+												id: 'cashier',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_handyman.png',
+												id: 'handyman',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d_typist.png',
+												id: 'typist',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'newtask'),
+													createClickableDiv(262,340,75,28, 'meeting_time'),
+													createClickableDiv(263,427,73,26, 'location'),
+													createClickableDiv(44,556,306,46, 'request_sent'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d2_location.png',
+												id: 'location',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'mover'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/d2_meetingtime.png',
+												id: 'meeting_time',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'mover'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/b_billing information.png',
+												id: 'settings',
+												clickableDivs: [
+													createClickableDiv(48,157,32,31, 'taskmenu'),
+												]
+											}),
+										new State({
+												img_src: 'images/assigner/f_profileRoger Copy 1.png',
+												id: 'request_sent',
+												clickableDivs: [
+													createClickableDiv(202,395,123,33, 'results'),
+													createClickableDiv(71,397,117,31, 'taskmenu'),
+												],
+											}),
+										new State({
+												img_src: 'images/assigner/e_results.png',
+												id: 'results',
+												clickableDivs: [
+													createClickableDiv(39,223,317,102, 'roger'),
+												],
+											}),
 
-									]
+										new State({
+												img_src: 'images/assigner/f_profileRoger.png',
+												id: 'roger',
+												clickableDivs: [
+													createClickableDiv(52,568,136,25, 'select_roger'),
+													createClickableDiv(211,569,128,27, 'contact_roger'),
+												],
+											}),
+										new State({
+												img_src: 'images/assigner/last_screen.png',
+												id: 'select_roger',
+												clickableDivs: [
+													createClickableDiv(64,390,127,41, 'taskmenu'),
+													createClickableDiv(199,392,124,39, 'contact_roger'),
+												],
+											}),
+										new State({
+												img_src: 'images/assigner/f_profileRoger Copy.png',
+												id: 'contact_roger',
+												clickableDivs: [
+													createClickableDiv(67,392,125,39, 'call'),
+													createClickableDiv(199,392,128,41, 'text'),
+												],
+											}),
+										new State({
+												img_src: 'images/assigner/g_call.png',
+												id: 'call',
+												clickableDivs: [
+													createClickableDiv(55,539,284,49, 'app_start'),
+												],
+											}),
+										new State({
+												img_src: 'images/assigner/g_text.png',
+												id: 'text',
+												clickableDivs: [
+													createClickableDiv(49,158,35,32, 'contact_roger'),
+												],
+											}),
+									],
+								firstState: 'app_start'
 							});
+		/*
+												onExit: function() {
+													$("div#iPhone").css("-webkit-transform", "rotate(720deg)");
+												}
+												*/
 		$(window).bind('hashchange', function(evt) {
 				var hash = window.location.hash.substr(1);
 				if(hash != fsm.state.id) { // The hash doesn't match the page
@@ -104,7 +283,6 @@ var paging = {
 }
 
 startclick = null;
-debug = false;
 
 var createClickableDiv = function(x, y, w, h, toState) {
 	var rv = $("<div />").css("position", "absolute")
@@ -127,12 +305,31 @@ var State = function(options) {
 };
 
 var FSM = function(options) {
+	var self = this;
 	this.options = options;
 	this.display = this.options.display;
 	this.states = this.options.states;
 
 	this.img = $("<img />").appendTo(this.display);
 	if(debug) {
+		function getX(oElement) {
+			var iReturnValue = 0;
+			while( oElement != null ) {
+				iReturnValue += oElement.offsetLeft;
+				oElement = oElement.offsetParent;
+			}
+			return iReturnValue;
+		}
+
+		function getY(oElement) {
+			var iReturnValue = 0;
+			while( oElement != null ) {
+				iReturnValue += oElement.offsetTop;
+				oElement = oElement.offsetParent;
+			}
+			return iReturnValue;
+		}
+		
 		$(window).mousedown(function(evt) {
 							startclick = [evt.clientX, evt.clientY];
 							rectDiv = $("<div />").css("opacity", 0.8)
@@ -157,11 +354,13 @@ var FSM = function(options) {
 							var startY = startclick[1];
 							var w = evt.pageX - startX;
 							var h = evt.pageY - startY;
+							startX -= getX(self.img[0]);
+							startY -= getY(self.img[0]);
 							startclick = null;
 							window.setTimeout(function(){
 								rectDiv.remove();
 								}, 1000);
-							console.log(startX+", "+startY+", "+w+", "+h);
+							console.log(startX+","+startY+","+w+","+h+",");
 						});
 	}
 	this.setState(this.options.firstState);
